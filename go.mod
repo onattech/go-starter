33ed3196f8cbebc4f2b70838830d5f138ee45a56
Onat <53895969+onattech@users.noreply.github.com>
2022-06-10 22:56:12 +0300

    Project initialization locally is ready

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/onattech/go-starter
 
 go 1.18
+
+require github.com/AlecAivazis/survey/v2 v2.3.5
+
+require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	golang.org/x/sys v0.0.0-20220608164250-635b8c9b7f68 // indirect
+	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,99 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/AlecAivazis/survey/v2"
+)
+
+// the questions to ask
+var qs = []*survey.Question{
+	{
+		Name:     "name",
+		Prompt:   &survey.Input{Message: "What is the project name?", Default: "go-my-project"},
+		Validate: survey.Required,
+	},
+	{
+		Name: "path",
+		Prompt: &survey.Select{
+			Message: "Choose the path:",
+			Options: []string{"~/coding/myProjects/GO", "~", "~/Desktop"},
+			Default: "~/coding/myProjects/GO",
+		},
+	},
+	// {
+	// 	Name:   "age",
+	// 	Prompt: &survey.Input{Message: "How old are you?"},
+	// },
+}
+
 func main() {
+	// the answers will be written to this struct
+	answers := struct {
+		Name string // survey will match the question and field names
+		Path string `survey:"path"` // or you can tag fields to match a specific name
+		// Age  int    // if the types don't match, survey will convert it
+	}{}
+
+	// perform the questions
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Printf("%+v\n", answers)
+
+	// Make project folder
+	err = os.Mkdir("/home/loku/coding/myProjects/GO/"+answers.Name, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Add main.go file
+	s := []byte("package main\n\nfunc main() {\n}")
+	ioutil.WriteFile("/home/loku/coding/myProjects/GO/"+answers.Name+"/main.go", s, 0755)
+
+	// Go mode init
+	cmd := exec.Command("go", "mod", "init", "github.com/onattech/"+answers.Name)
+	cmd.Dir = "/home/loku/coding/myProjects/GO/" + answers.Name
+	cmd.Run()
+
+	// Git init
+	cmd = exec.Command("git", "init")
+	cmd.Dir = "/home/loku/coding/myProjects/GO/" + answers.Name
+	cmd.Run()
+
+	// Add .gitignore file
+	s = []byte(`# Binaries for programs and plugins
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+
+# Test binary, built with 'go test -c'
+*.test
+
+# Output of the go coverage tool, specifically when used with LiteIDE
+*.out
+
+# Dependency directories (remove the comment below to include it)
+# vendor/
+	`)
+	ioutil.WriteFile("/home/loku/coding/myProjects/GO/"+answers.Name+"/.gitignore", s, 0644)
+
+	// Git add all
+	cmd = exec.Command("git", "add", ".")
+	cmd.Dir = "/home/loku/coding/myProjects/GO/" + answers.Name
+	cmd.Run()
+
+	// Git initial commit
+	cmd = exec.Command("git", "commit", "-m", "\"Initial commit\"")
+	cmd.Dir = "/home/loku/coding/myProjects/GO/" + answers.Name
+	cmd.Run()
+
 }
